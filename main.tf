a2876e401037f3f5964aea31206058c07476e19f
megha <anmeghan04@gmail.com>
2024-04-23 19:34:32 +0530

    main

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
 #create s3 bucket
-resource "aws_s3_bucket" "mybucket" {
+resource "aws_s3_bucket" "charanbucket" {
   bucket = var.bucketname
 }
--- variables.tf
+++ variables.tf
@@ -1,3 +1,3 @@
 variable "bucketname" {
-  default = "myterraformproject2024"
+  default = "charanwebsite"
 }
